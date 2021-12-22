669aa5941fc45f565c495bf826f14f449c92466c
Verevkina Elena <verevkinaee@gmail.com>
2021-12-22 19:24:47 +0200

    add changes in the structure

--- service/__init__.py
+++ service/__init__.py
@@ -1,3 +1,5 @@
-from .genres_service import GenresService
+from service.genre import GenresService
+from service.director import DirectorsService
+from service.movie import MoviesService
 
-__all__ = ["GenresService"]
+__all__ = ["GenresService", "DirectorsService", "MoviesService"]
