58b762a32559f1693a59e3481b4b96f2da97ddd9
Verevkina Elena <verevkinaee@gmail.com>
2022-01-11 11:13:53 +0200

    add changes

--- project/dao/auth.py
+++ project/dao/auth.py
@@ -1,12 +1,14 @@
 from project.dao.base import BaseDao
 from project.dao.models.user import User
+from project.exceptions import ItemNotFound
 
 
 class AuthDAO(BaseDao):
     def create(self, **new_user):
-        if self._db_session.query(User).filter(User.email == new_user["email"]).first():
-            return "error"
+        self._db_session.query(User).filter(User.email == new_user["email"]).first()
         new_user = User(**new_user)
+        if not new_user:
+            return ItemNotFound
         self._db_session.add(new_user)
         self._db_session.commit()
         return new_user
